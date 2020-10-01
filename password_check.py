b57f8f19f474479a4da7ae119d3b78c2db4b5db1
Joel John <joeljo2104@gmail.com>
2020-10-01 17:03:58 +0400

    Design Colour Changes

--- password_check.py
+++ password_check.py
@@ -1,20 +1,25 @@
 from tkinter import *
 import re
 
-gui=Tk()
-gui.geometry('500x100+700+250')
+gui = Tk()
+gui.geometry('500x400+700+250')
 gui.title('PASSWORD GENERATOR')
-guiFont= font = dict(family='Courier New, monospaced',size=18,color='#7f7f7f')
-eLabel = Label(gui,text="Enter your Password:  ",font=guiFont)
-eLabel.grid(row=0,column=0)
-ePass = Entry(gui,show="*")
-ePass.grid(row=0,column=1)
+gui.config(bg='blue')
+guiFont = font = dict(family='Courier New, monospaced',
+                      size=18, color='#ffffff')
+eLabel = Label(gui, text="Enter your Password:  ", font=guiFont, bg='blue', fg='white')
+eLabel.grid(row=0, column=0)
+ePass = Entry(gui, show="*")
+ePass.grid(row=0, column=1)
+
 
 def checkPass():
-    str=['password cannot be Blank','very weak','weak','medium','strong','very strong']
+    passwordStr = StringVar()
+    str = ['password cannot be Blank', 'very weak',
+           'weak', 'medium', 'strong', 'very strong']
     count = 1
     password = ePass.get()
-    print(password,len(password))
+    print(password, len(password))
 
     if len(password) == 0:
         passwordStr.set(str[0])
@@ -24,17 +29,18 @@ def checkPass():
         return
     if len(password) > 8:
         count += 1
-    if re.search("[0-9]",password):
+    if re.search("[0-9]", password):
         count += 1
-    if re.search("[a-z]",password) and re.search("[A-Z]",password):
+    if re.search("[a-z]", password) and re.search("[A-Z]", password):
         count += 1
-    if re.search(".",password):
+    if re.search(".", password):
         count += 1
-    passwordStr.str(str[count])
+    passwordStr.set(str[count])
+    checkStrLab = Label(gui, textvariable=passwordStr, bg='blue', fg='white')
+    checkStrLab.grid(row=2, column=1, sticky='w')
 
-    passwordStr = StringVar()
-    checkStrBtn = Button(gui,text="Check", command=checkPass,height=2,width=25,font=guiFont)
-    checkStrBtn.grid(row=2,column=0)
-    checkStrLab = Label(gui,textvariable=passwordStr)
-    checkStrLab.grid(row=2,column=1,sticky=w)
-    gui.mainloop()       
+checkStrBtn = Button(gui, text="Check", command=checkPass,
+                        height=2, width=25, font=guiFont, bg='dark blue', fg='white')
+checkStrBtn.grid(row=2, column=0)
+
+gui.mainloop()
